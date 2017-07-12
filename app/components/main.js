2f14964581c24eb0b967ddeb0a2adc3579b3592e
Sarah Amirie <asamirie@gmail.com>
2017-07-12 16:08:43 -0400

    googleapi

---
+++ app/components/main.js
@@ -0,0 +1,19 @@
+// Include React
+import React, { Component } from "react";
+import helper from '../../utils/helper';
+// here is where the header and input box go
+class Main extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return(
+      <h2>Give</h2>
+    )
+  }
+}
+export default Main;
---
+++ server.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var passport = require("passport");
+var session = require('express-session');
+mongoose.Promise = Promise;
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+require("./auth/passport")(passport);
+app.use(passport.initialize());
+app.use(passport.session());
+require("./routes/users")(app, passport);
+
+app.get("*",function(req, res){
+	res.sendFile(path.join(__dirname, "./index.html"));
+});
+
+mongoose.connect('mongodb://localhost/todo');
+mongoose.connection.on('connected', function () {
+  console.log("Mongoose connected to MongoDB");
+});
+mongoose.connection.on('error', function (err) {
+  console.log("Mongoose error: " + err);
+});
+
+app.listen(PORT, function(req, res){
+	console.log("Listening on port: " + PORT);
+});
